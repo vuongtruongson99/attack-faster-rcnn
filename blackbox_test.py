8b57fd295f44dcfce195bb83841f7c2c867b0db8
Joey Bose <joey.bosr@gmail.com>
2018-06-05 19:43:36 -0400

    small changes to test

--- blackbox_test.py
+++ blackbox_test.py
@@ -100,43 +100,6 @@ class FaceLandmarksDataset(Dataset):
 	img = torch.from_numpy(img)[None]
         return img,self.g_images[idx]
 
-def preprocess(img, min_size=600, max_size=1000):
-    """Preprocess an image for feature extraction.
-
-    The length of the shorter edge is scaled to :obj:`self.min_size`.
-    After the scaling, if the length of the longer edge is longer than
-    :param min_size:
-    :obj:`self.max_size`, the image is scaled to fit the longer edge
-    to :obj:`self.max_size`.
-
-    After resizing the image, the image is subtracted by a mean image value
-    :obj:`self.mean`.
-
-    Args:
-        img (~numpy.ndarray): An image. This is in CHW and RGB format.
-            The range of its value is :math:`[0, 255]`.
-         (~numpy.ndarray): An image. This is in CHW and RGB format.
-            The range of its value is :math:`[0, 255]`.
-
-    Returns:
-        ~numpy.ndarray:
-        A preprocessed image.
-
-    """
-    C, H, W = img.shape
-    scale1 = min_size / min(H, W)
-    scale2 = max_size / max(H, W)
-    scale = min(scale1, scale2)
-    img = img / 255.
-    try:
-        img = sktsf.resize(img, (C, H * scale, W * scale), mode='reflect')
-    except:
-        ipdb.set_trace()
-    # both the longer and shorter should be less than
-    # max_size and min_size
-    normalize = pytorch_normalze
-    return normalize(img)
-
 def add_bbox(ax,bbox,label,score):
     for i, bb in enumerate(bbox):
         xy = (bb[1], bb[0])
@@ -191,15 +154,10 @@ if __name__ == '__main__':
         trainer.faster_rcnn.score_thresh = quality
         for i in range(len(_data)):
             img,im_path = _data[i]
-            ipdb.set_trace()
-            img = read_image('/home/joey/Desktop/simple-faster-rcnn-pytorch/reporter.jpg')
-            img = preprocess(img)
-            img = torch.from_numpy(img)[None]
-            im_path = '/home/joey/Desktop/simple-faster-rcnn-pytorch/reporter.jpg'
             img = Variable(img.float().cuda())
             im_path_clone = b = '%s' % im_path
             save_path = _data.save_dir + 'frcnn' + im_path.split('/')[-1]
-            save_path_adv = _data.save_dir_adv + 'frcnn_adv_' + im_path_clone.split('/')[-1]
+            save_path_adv = _data.save_dir_adv + 'frcnn_adv/' + im_path_clone.split('/')[-1]
             save_path_combined = _data.save_dir_comb + 'frcnn_comb_' + im_path_clone.split('/')[-1]
             save_path_perturb = _data.save_dir_perturb + 'frcnn_perturb_' + im_path_clone.split('/')[-1]
             if not os.path.exists(_data.save_dir):
@@ -213,17 +171,11 @@ if __name__ == '__main__':
             ori_img_ = inverse_normalize(at.tonumpy(img[0]))
             _bboxes, _labels, _scores = trainer.faster_rcnn.predict([ori_img_],\
                     new_score=quality,visualize=True)
-            adv_img,perturb = trainer.attacker.perturb(img,epsilon=0.1,save_perturb=save_path_perturb)
+            adv_img,perturb = trainer.attacker.perturb(img,save_perturb=save_path_perturb)
             adv_img_ = inverse_normalize(at.tonumpy(adv_img[0]))
             perturb = inverse_normalize(at.tonumpy(perturb[0]))
             perturb = perturb.transpose((1,2,0))
-            diff = np.clip(adv_img_ - ori_img_,0,255)
-            perturb = np.clip(1.1*perturb, 0, 255)
-            cv2.imwrite('reporter_adv.png',cv2.cvtColor(adv_img_.transpose((1,2,0)), \
-                cv2.COLOR_RGB2BGR))
-            rgbImg = cv2.imread('dlib_adv.png')
-            rgbImg = rgbImg.transpose((2,1,0))
-            # cv2.imwrite(save_path_perturb,perturb)
+            cv2.imwrite(save_path_perturb,perturb)
             # jpg_adv_img_ = img2jpg(adv_img_,im_path_clone.split('/')[-1],quality)
             adv_bboxes, adv_labels, adv_scores = trainer.faster_rcnn.predict([adv_img_], \
                     new_score=quality,visualize=True,adv=True)
@@ -238,32 +190,18 @@ if __name__ == '__main__':
                 detected += 1
             # print("Quality %d Detected Adversarial faces %d, JPG faces %d out of %d" \
                         # %(quality,adv_detected,jpg_detected,i))
-            ori_img_ = ori_img_.transpose((1,2,0))
-            adv_img_ = adv_img_.transpose((1,2,0))
-            diff = diff.transpose((1,2,0))
             print("Detected %d faces and %d Adv Faces out of %d" %(detected,adv_detected,i))
-            # fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True,
-                    # sharex=True,figsize=(6,8))
-            fig, (ax1) = plt.subplots(1, 1, sharey=True,
-                    sharex=True)
-            ax1.axis('off')
-            ax1 = add_bbox(ax1,at.tonumpy(_bboxes[0]),at.tonumpy(_labels[0]),\
-                    at.tonumpy(_scores[0]))
-            ax1.imshow(ori_img_.astype(np.uint8))
-            fig.savefig(save_path,bbox_inches='tight',pad_inches=0)
-            fig2, (ax2) = plt.subplots(1, 1, sharey=True,
-                    sharex=True)
-            ax2.axis('off')
-            ax2 = add_bbox(ax2,at.tonumpy(adv_bboxes[0]),at.tonumpy(adv_labels[0]),\
-                    at.tonumpy(adv_scores[0]))
-            ax2.imshow(adv_img_.astype(np.uint8))
-            fig2.savefig(save_path_adv,bbox_inches='tight',pad_inches=0)
-            fig3, (ax3) = plt.subplots(1, 1, sharey=True,
-                    sharex=True)
-            ax3.axis('off')
-            ax3.imshow(diff.astype(np.uint8))
-            fig3.savefig(save_path_perturb,bbox_inches='tight',pad_inches=0)
-            ipdb.set_trace()
+
+            # fig, (ax1, ax2) = plt.subplots(1, 2), sharey=True, sharex=True)
+            # ax1.axis('off')
+            # ax2.axis('off')
+            # ax1.imshow(ori_img_.astype(np.uint8))
+            # ax2.imshow(adv_img_.astype(np.uint8))
+            # ax1 = add_bbox(ax1,at.tonumpy(_bboxes[0]),at.tonumpy(_labels[0]),\
+                    # at.tonumpy(_scores[0]))
+            # ax2 = add_bbox(ax2,at.tonumpy(adv_bboxes[0]),at.tonumpy(adv_labels[0]),\
+                    # at.tonumpy(adv_scores[0]))
+            # fig.savefig(save_path_combined,bbox_inches='tight',pad_inches=0)
             # ori_img_ = ori_img_.transpose((1,2,0))
             # adv_img_ = adv_img_.transpose((1,2,0))
             # cv2.imwrite(save_path,ori_img_)
